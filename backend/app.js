005a08bb10aa9b71c9aed29d26e64b222762aa49
Peter Kovacs <60811413+kovacs-peter@users.noreply.github.com>
2025-03-02 17:06:34 +0100

    ci: config eslint for backend

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const itemsRouter = require('./routes/items');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const itemsRouter = require("./routes/items");
 
 const app = express();
 
 // Configure CORS
-const allowedOrigins = ['http://localhost:4200'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+const allowedOrigins = ["http://localhost:4200"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/items', itemsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/items", itemsRouter);
 
 module.exports = app;
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 let items = [
@@ -12,47 +12,47 @@ let items = [
   },
 ];
 
-router.post('/', function(req, res) {
+router.post("/", function (req, res) {
   const newItem = req.body;
   newItem.id = items.length ? items[items.length - 1].id + 1 : 1;
   items.push(newItem);
   res.status(201).json(newItem);
 });
 
-router.get('/', function(req, res) {
-  const resItems =  items.map((item)=> ({
+router.get("/", function (req, res) {
+  const resItems = items.map((item) => ({
     ...item,
-    imageUrl: "https://placehold.co/150x150"
-  }))
-  res.json(resItems)
+    imageUrl: "https://placehold.co/150x150",
+  }));
+  res.json(resItems);
 });
 
-router.get('/:id', function(req, res) {
-  const item = items.find(i => i.id === parseInt(req.params.id));
+router.get("/:id", function (req, res) {
+  const item = items.find((i) => i.id === parseInt(req.params.id));
   if (item) {
     res.json(item);
   } else {
-    res.status(404).send('Item not found');
+    res.status(404).send("Item not found");
   }
 });
 
-router.put('/:id', function(req, res) {
-  const item = items.find(i => i.id === parseInt(req.params.id));
+router.put("/:id", function (req, res) {
+  const item = items.find((i) => i.id === parseInt(req.params.id));
   if (item) {
     Object.assign(item, req.body);
     res.json(item);
   } else {
-    res.status(404).send('Item not found');
+    res.status(404).send("Item not found");
   }
 });
 
-router.delete('/:id', function(req, res) {
-  const index = items.findIndex(i => i.id === parseInt(req.params.id));
+router.delete("/:id", function (req, res) {
+  const index = items.findIndex((i) => i.id === parseInt(req.params.id));
   if (index !== -1) {
     const deletedItem = items.splice(index, 1);
     res.json(deletedItem);
   } else {
-    res.status(404).send('Item not found');
+    res.status(404).send("Item not found");
   }
 });
 
