5630ee0fe4592a888327a002fef8f761085d85f3
Peter Kovacs <60811413+kovacs-peter@users.noreply.github.com>
2025-03-02 16:14:58 +0100

    fix: tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { ItemsStore } from "./store/items.store";
 
 describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
+      providers: [ItemsStore],
     }).compileComponents();
   });
 
--- frontend/src/app/pages/items-list/list-item/list-item.component.spec.ts
+++ frontend/src/app/pages/items-list/list-item/list-item.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ListItemComponent } from "./list-item.component";
+import { ItemsStore } from "../../../store/items.store";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
 
 describe("ListItemComponent", () => {
   let component: ListItemComponent;
@@ -9,10 +12,29 @@ describe("ListItemComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ListItemComponent],
+      providers: [
+        ItemsStore,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 1 }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListItemComponent);
     component = fixture.componentInstance;
+
+    component.item = {
+      id: "1",
+      name: "Test Item",
+      imageUrl: "http://example.com/image.jpg",
+      description: "Test description",
+      unitPrice: 100,
+      quantity: 100,
+    };
+
     fixture.detectChanges();
   });
 
